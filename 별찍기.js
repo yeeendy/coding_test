bf02d0dc240ed1799228174c48fa8ddd28a23609
yeeendy <124010808+yeeendy@users.noreply.github.com>
2023-02-25 20:50:19 +0900

    Add files via upload

---
+++ 별찍기.js
@@ -0,0 +1,7 @@
+let n=5;
+for(let i=1;i<=n;i++){
+    for(let j=1;j<=i;j++){
+        process.stdout.write('*');
+    }
+    console.log();
+}
---
+++ 합.js
@@ -0,0 +1,6 @@
+let n=3;
+let result=0;
+for(let i=1;i<=n;i++){
+    result+=i;
+}
+console.log(result);
